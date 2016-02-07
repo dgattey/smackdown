66f055bcb38e8de636b569c251e60671c944f7d9
samdallas <samdallas100@gmail.com>
2016-02-06 19:55:33 -0500

    fixed the score table stuff

--- backend/scorekeeping.py
+++ backend/scorekeeping.py
@@ -7,9 +7,11 @@ import time
 # number of tweets, and number of scores. 
 num_intervals = 3 	# how many time intervals do we keep data for?
 interval = 60 		# start using 1 minute as a time interval
-score_table_a = [[0, 0]]*num_intervals	# each entry in this table is [# of tweets, total score]
+#score_table_a = [[0, 0]]*num_intervals	# each entry in this table is [# of tweets, total score]
+# not recording the number of tweets now. Each entry is the total score for that interval
+score_table_a = [0]*num_intervals
 last_update = 0
-score_table_b = [[0, 0]]*num_intervals	
+score_table_b = [0]*num_intervals	
 
 
 # Input: the new score of the tweet
@@ -23,39 +25,44 @@ def add_score(score, team):
 	if time.time() > (last_update + interval):
 		last_update = time.time()
 		score_table_a = score_table_a[1:]
-		score_table_a.append([0, 0])
+		score_table_a.append(0)
 		score_table_b = score_table_b[1:]
-		score_table_b.append([0, 0])
+		score_table_b.append(0)
 	# now update the current scores
 	if team == 'a':
-		score_table_a[-1][0] += 1
-		score_table_a[-1][1] += score
+		score_table_a[-1] += score
 	if team == 'b':
-		score_table_b[-1][0] += 1
-		score_table_b[-1][1] += score
-	#print score_table_a
-	#print score_table_b
+		score_table_b[-1] += score
+	score_now()
 
 
 # outputs a number from 0 to 100 that the dial should have
 # score is calculated based on data from the last minute.
 def score_now():
 	# create a scale -> 0 is at team a, 100 is at team b
-	a = team_a[-1][1]
-	b = team_b[-1][1]
-	diff = team_a - team_b
-	tot = team_a + team_b
-	return (0.5 + (diff/(2*tot)))*100
-
-
+	a = 0
+	b = 0
+	for i in range(num_intervals):
+		a += score_table_a[i]
+		b += score_table_b[i]
+	a = float(a)
+	b = float(b)
+	diff = a - b
+	tot = a + b
+	return ((b/(a+b))*100)
 
 """
-# takes the raw scores for teams a and b, 
-# outputs the number from 0 to 100 that the dial should have
-def normalize(team_a, team_b):
-	# create a scale -> 0 is at team a, 100 is at team b
-	diff = team_a - team_b
-	tot = team_a + team_b
-	return (0.5 + (diff/(2*tot)))*100
+if __name__ == "__main__":
+	add_score(2, 'a')
+	score_now()
+	add_score(2, 'a')
+	score_now()
+	add_score(9, 'a')
+	add_score(1, 'b')
+	
+	score_now()
+	add_score(3, 'b')
+	score_now()
+
 """
 

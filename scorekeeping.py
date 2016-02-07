9cc6913641b91ce549d990a2ee0604d7ee154ed8
samdallas <samdallas100@gmail.com>
2016-02-06 20:08:37 -0500

    added a score history, add_score takes in tweets for both teams

--- scorekeeping.py
+++ scorekeeping.py
@@ -13,32 +13,33 @@ score_table_a = [0]*num_intervals
 last_update = 0
 score_table_b = [0]*num_intervals	
 
+# history - for as long as the app has been running, it collects data
+history = []		# this is an array of tuples
+
 
 # Input: the new score of the tweet
-# the team that is doing the smacking. - CHECK THIS
-def add_score(score, team):
+def add_score(score_a, score_b):
 	# check to see if it's been more than an interval since the last update
 	# if it's been longer than an interval, need to update the score table
 	global last_update
 	global score_table_a
 	global score_table_b
-	if time.time() > (last_update + interval):
-		last_update = time.time()
+	now = time.time()
+	if now > (last_update + interval):
+		last_update = now
 		score_table_a = score_table_a[1:]
 		score_table_a.append(0)
 		score_table_b = score_table_b[1:]
 		score_table_b.append(0)
 	# now update the current scores
-	if team == 'a':
-		score_table_a[-1] += score
-	if team == 'b':
-		score_table_b[-1] += score
-	score_now()
+	score_table_a[-1] += score_a
+	score_table_b[-1] += score_b
+	score_now(now)
 
 
 # outputs a number from 0 to 100 that the dial should have
 # score is calculated based on data from the last minute.
-def score_now():
+def score_now(time):
 	# create a scale -> 0 is at team a, 100 is at team b
 	a = 0
 	b = 0
@@ -47,22 +48,7 @@ def score_now():
 		b += score_table_b[i]
 	a = float(a)
 	b = float(b)
-	diff = a - b
-	tot = a + b
-	return ((b/(a+b))*100)
-
-"""
-if __name__ == "__main__":
-	add_score(2, 'a')
-	score_now()
-	add_score(2, 'a')
-	score_now()
-	add_score(9, 'a')
-	add_score(1, 'b')
-	
-	score_now()
-	add_score(3, 'b')
-	score_now()
-
-"""
+	score = ((b/(a+b))*100)
+	history.append((time, score))
+	return score
 
